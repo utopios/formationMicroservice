6e8518967e69659ade4ae2ef9570f730924006e1
ihab <ihab@utopios.net>
2021-06-24 15:16:38 +0100

    exemple API

---
+++ simple-api-gateway/api-gateway/src/address.config.ts
@@ -0,0 +1,4 @@
+export const addressesMap = {
+    'customer' : 'http://localhost:3001',
+    'order' : 'http://localhost:3002',
+}
---
+++ simple-api-gateway/api-gateway/src/app.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import axios from "axios"
+import { addressesMap } from './address.config';
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get('/app-1')
+  // async getApp1() {
+  //   const result = await axios.get('http://localhost:3001/')
+  //   return result.data
+  // } 
+  // @Get('/app-2')
+  // async getApp2() {
+  //   const result = await axios.get('http://localhost:3002/')
+  //   return result.data
+  // }
+
+  // @Post('/app-1')
+  // async postApp1(@Body() data:any) {
+  //   const result = await axios.post('http://localhost:3001', {...data})
+  //   return result.data
+  // }
+
+  // @Post('/app-2')
+  // async postApp2(@Body() data:any, @Req() request: Request) {
+  //   const result = await axios.post('http://localhost:3002', {...data}, {headers : request.headers})
+  //   return result.data
+  // }
+
+  @Get(':api')
+  async getApi(@Param() params) {
+      const result = await axios.get(addressesMap[params.api])
+      return result.data
+  }
+
+  @Post(':api')
+  async postApi(@Param() params, @Body() data:any) {
+      const result = await axios.get(addressesMap[params.api], data)
+      return result.data
+  }
+}
